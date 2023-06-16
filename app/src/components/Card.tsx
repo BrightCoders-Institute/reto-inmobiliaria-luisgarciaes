e97c0dce8d7c516493a10b367658dddf565c0684
ruruisu <zaddozgl@gmail.com>
2023-06-16 16:59:29 -0600

    item and list component

--- app/src/components/Card.tsx
+++ app/src/components/Card.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { ImageBackground, Text, View } from 'react-native';
 
-type ImageCardProps = {
+type Card = {
   img: string;
   stars: string;
 };
 
-const ImageCard: React.FC<ImageCardProps> = ({ img, stars }) => {
+const Card: React.FC<Card> = ({ img, stars }) => {
   return (
     <ImageBackground source={{ uri: img }}>
       <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', height: 15, width: 25, borderRadius: 10, margin: 5 }}>
@@ -16,4 +16,4 @@ const ImageCard: React.FC<ImageCardProps> = ({ img, stars }) => {
   );
 };
 
-export default ImageCard;
+export default Card;
---
+++ app/src/components/Item.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { Property } from '../types/PropertyType';
+import { Text, TouchableOpacity, View } from 'react-native';
+import ImageCard from './Card';
+
+type ItemCardProps = {
+  property: Property;
+};
+
+const ItemCard: React.FC<ItemCardProps> = ({ property }) => {
+  const [like, setLike] = useState(property.like);
+  return (
+    <View>
+      <ImageCard img={property.img} stars={property.stars} />
+      <View>
+        <Text>{property.name}</Text>
+        <Text>{property.address}</Text>
+        <Text>{property.rooms}</Text>
+        <Text>{property.bathrooms}</Text>
+        <Text>{property.size}</Text>
+        <Text>${property.price}</Text>
+        <TouchableOpacity
+          onPress={() => setLike(!like)}
+        >
+          <Text>{like ? 'Liked' : 'Like'}</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default ItemCard;
