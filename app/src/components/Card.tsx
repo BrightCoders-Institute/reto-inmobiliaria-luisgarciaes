b0390577e1fc2518366689a4c85ecbee5dadc421
ruruisu <zaddozgl@gmail.com>
2023-06-16 18:33:54 -0600

    styles

--- app/src/components/Card.tsx
+++ app/src/components/Card.tsx
@@ -10,7 +10,7 @@ const Card: React.FC<Card> = ({ img, stars }) => {
   return (
     <ImageBackground source={{ uri: img }}>
       <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', height: 15, width: 25, borderRadius: 10, margin: 5 }}>
-        <Text style={{ color: '#856D35', marginLeft: 1, fontSize: 12}}>{stars}</Text>
+        <Text style={{ color: 'black', marginLeft: 1, fontSize: 12}}>{stars}</Text>
       </View>
     </ImageBackground>
   );
--- app/src/components/Item.tsx
+++ app/src/components/Item.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Property } from '../property/property';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { Text, TouchableOpacity, View, StyleSheet } from 'react-native';
 import ImageCard from './Card';
 
 type ItemCardProps = {
@@ -10,23 +10,74 @@ type ItemCardProps = {
 const ItemCard: React.FC<ItemCardProps> = ({ property }) => {
   const [like, setLike] = useState(property.like);
   return (
-    <View>
+    <View style={styles.container}>
       <ImageCard img={property.img} stars={property.stars} />
-      <View>
-        <Text>{property.name}</Text>
-        <Text>{property.address}</Text>
-        <Text>{property.rooms}</Text>
-        <Text>{property.bathrooms}</Text>
-        <Text>{property.size}</Text>
-        <Text>${property.price}</Text>
+      <View style={styles.propertyInfo}>
+        <Text style={styles.propertyName}>{property.name}</Text>
+        <Text style={styles.propertyAddress}>{property.address}</Text>
+        <Text style={styles.propertyDetails}>{property.rooms} rooms</Text>
+        <Text style={styles.propertyDetails}>{property.bathrooms} bathrooms</Text>
+        <Text style={styles.propertyDetails}>Size: {property.size}</Text>
+        <Text style={styles.propertyPrice}>${property.price}</Text>
         <TouchableOpacity
+          style={styles.likeButton}
           onPress={() => setLike(!like)}
         >
-          <Text>{like ? 'Liked' : 'Like'}</Text>
+          <Text style={styles.likeButtonText}>{like ? 'Liked' : 'Like'}</Text>
         </TouchableOpacity>
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 20,
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  propertyInfo: {
+    padding: 16,
+  },
+  propertyName: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  propertyAddress: {
+    fontSize: 14,
+    color: '#666',
+    marginBottom: 8,
+  },
+  propertyDetails: {
+    fontSize: 14,
+    marginBottom: 4,
+  },
+  propertyPrice: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 8,
+  },
+  likeButton: {
+    marginTop: 16,
+    paddingVertical: 8,
+    paddingHorizontal: 12,
+    backgroundColor: '#f5f5f5',
+    borderRadius: 4,
+    alignItems: 'center',
+  },
+  likeButtonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
 
 export default ItemCard;
